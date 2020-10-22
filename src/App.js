4ad2b163753c0cf69a2a5a7559d861ebe08a37ae
Callum Woon <callumwoon@me.com>
2020-10-22 15:07:16 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <button>
+          Change text
+        </button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
