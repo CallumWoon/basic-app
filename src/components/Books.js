3c41df633e3e5d6bdd2c0be96c7512dcf05ab9b5
Callum Woon <callumwoon@me.com>
2020-10-24 15:15:39 +0100

    implemented react router

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,16 +1,18 @@
 import React from 'react';
 
 const Books = () => {
-  <div className="main-content courses">
-    <div className="books-header group">
-      <h2>Books</h2> 
-      <ul className="books-nav">
-        <li><a href='/books/bookone'>Book 1</a></li>
-        <li><a href='/books/booktwo'>Book 2</a></li>
-        <li><a href='/books/bookthree'>Book 3</a></li>
-      </ul>
+  return (
+    <div className="main-content courses">
+      <div className="books-header group">
+        <h2>Books</h2> 
+        <ul className="books-nav">
+          <li><a href='/books/bookone'>Book 1</a></li>
+          <li><a href='/books/booktwo'>Book 2</a></li>
+          <li><a href='/books/bookthree'>Book 3</a></li>
+        </ul>
+      </div>
     </div>
-  </div>
+  )  
 }
 
 export default Books;
