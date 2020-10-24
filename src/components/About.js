f3cff71dc1a2c2c7f92505b795bbe08d40234890
Callum Woon <callumwoon@me.com>
2020-10-24 15:22:46 +0100

    implement-inline-render

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const About = () => {
+const About = (props) => {
   return (
     <div className="main-content">
-      <h2>About</h2>
+      <h2>{props.title}</h2>
       <p>Sportsman do offending supported extremity breakfast by listening. Decisively advantages nor expression unpleasing she led met. Estate was tended ten boy nearer seemed. As so seeing latter he should thirty whence. Steepest speaking up attended it as. Made neat an on be gave show snug tore.</p>
     </div>
   )
